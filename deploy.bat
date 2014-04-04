fbb6344fdbf6d0a800c971d6a69674b86d72606a
sippeangelo <holmberg.simon@gmail.com>
2014-04-04 20:32:14 +0200

    Deploy script now also deploying shaders and textures.

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,8 @@
 SET AssetSource=%UserProfile%\Google Drive\Fisk_ Litet Spelprojekt\Assets
 
 ECHO Copying assets from %AssetSource%
-XCOPY "%AssetSource%" "assets\" /S /Y /Q
+RMDIR "assets\" /S /Q
+XCOPY "%AssetSource%" "assets\" /E /Y /Q
 CALL :deploy Debug
 CALL :deploy Release
 GOTO:eof
@@ -12,7 +13,12 @@ GOTO:eof
 :deploy
 ECHO Deploying %~1
 SET ConfigPath=bin\%~1
-MKLINK "%ConfigPath%\assets\" "assets\" /J
+:: Asset folders
+MKLINK "%ConfigPath%\models\" "assets\models" /J
+MKLINK "%ConfigPath%\textures\" "assets\textures\" /J
+:: Shaders
+MKLINK "%ConfigPath%\shaders\" "src\shaders\" /J
+:: DLLs
 MKLINK "%ConfigPath%\glfw3.dll" "libs\glfw-3.0.4\lib\%~1\glfw3.dll" /H
 MKLINK "%ConfigPath%\OpenAL32.dll" "libs\openal-soft-1.15.1\lib\Win32\%~1\OpenAL32.dll" /H
 IF %~1==Debug (
