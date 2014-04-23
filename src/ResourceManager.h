de1290bf67cbc96bfba2523f55f6544fbfb7ebc0
sippeangelo <holmberg.simon@gmail.com>
2014-04-23 17:58:11 +0200

    Added a few debug messages to ResourceManager.
    No longer warns about hot-loading when a resource which causes a load chain is preloaded.

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -19,14 +19,17 @@ class ResourceManager
 {
 public:
 	ResourceManager() 
-		: m_CurrentResourceTypeID(0) { }
+		: m_CurrentResourceTypeID(0), m_Preloading(false) { }
 
 	// Registers the factory function of a resource type
 	void RegisterType(std::string resourceType, std::function<Resource*(std::string)> factoryFunction);
 
 	// Loads a resource and caches it for future use
 	void Preload(std::string resourceType, std::string resourceName);
-	
+
+	// Checks if a resource is in cache
+	bool IsResourceLoaded(std::string resourceName);
+
 	template <typename T>
 	// Hot-loads a resource and caches it for future use
 	T* Load(std::string resourceType, std::string resourceName);
@@ -44,6 +47,8 @@ private:
 	std::unordered_map<std::string, unsigned int> m_ResourceTypeIDs;
 	// Number of resources of a type. Doubles as local ID.
 	std::unordered_map<unsigned int, unsigned int> m_ResourceCount;
+	// Flag to suppress hot-load warnings when a preloading resource chain loads another resource
+	bool m_Preloading;
 
 	unsigned int GetTypeID(std::string resourceType);
 	unsigned int GetNewResourceID(unsigned int typeID);
@@ -55,20 +60,34 @@ private:
 template <typename T>
 T* ResourceManager::Load(std::string resourceType, std::string resourceName)
 {
+	auto it = m_ResourceCache.find(resourceName);
+	if (it != m_ResourceCache.end())
+		return static_cast<T*>(it->second);
+
+	if (m_Preloading)
+	{
+		LOG_INFO("Preloading resource \"%s\"", resourceName.c_str());
+	}
+	else
+	{
+		LOG_WARNING("Hot-loading resource \"%s\"", resourceName.c_str());
+	}
+
 	return static_cast<T*>(CreateResource(resourceType, resourceName));
 }
 
 template <typename T>
 T* ResourceManager::Fetch(std::string resourceName) const
 {
-	if (m_ResourceCache.find(resourceName) == m_ResourceCache.end())
+	auto it = m_ResourceCache.find(resourceName);
+	if (it == m_ResourceCache.end())
 	{
 		LOG_ERROR("Failed to fetch resource \"%s\": Resource not loaded!", resourceName.c_str());
 		return nullptr;
 	}
 	else
 	{
-		return static_cast<T*>(m_ResourceCache.at(resourceName));
+		return static_cast<T*>(it->second);
 	}
 }
 
