dfd94254731eb00528702569520e1d54dcf71ed9
sippeangelo <holmberg.simon@gmail.com>
2014-04-23 02:19:17 +0200

    WIP Frame

--- src/GUI/Frame.h
+++ src/GUI/Frame.h
@@ -1,12 +1,31 @@
 #ifndef GUI_Frame_h__
 #define GUI_Frame_h__
 
+#include <memory>
+
+#include "Util/Rectangle.h"
+
 namespace GUI
 {
 	
-class Frame
+class Frame : public Rectangle
 {
+public:
+	enum class Anchor
+	{
+		Left,
+		Right,
+		Top,
+		Bottom
+	};
 
+	Frame() { }
+	
+	Frame(const Frame &parent)
+		: Rectangle(static_cast<Rectangle>(parent))
+		, Parent(std::shared_ptr<Frame>(&parent)) { }
+
+	std::shared_ptr<Frame> Parent;
 };
 
 }
